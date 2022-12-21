6dc78ab007c7c36c7abe79c0f8008fbb4ffa2b69
karinaakopyan <akopiankaryna@gmail.com>
2022-12-21 10:44:47 +0300

    opened the file in the necessary format

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+def create():
+    the_list = []
+    x = r"C:/Users/Admin/practice_it/data_file.csv"
+    with open(x,"r") as f:
+        reader= f.readlines()
+        for row in reader:
+            the_list.append(row.strip().split(","))
+    return the_list
