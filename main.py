91fc84300d6579136f217bd513d3068bb036cb69
karinaakopyan <akopiankaryna@gmail.com>
2022-12-21 10:54:36 +0300

    added medals counter

--- main.py
+++ main.py
@@ -19,3 +19,21 @@ def create_country():
     for i in the_list:
         if i[6]==sys.argv[4]:
             year.append(i[9])
+
+for i in the_list:
+        if i[6] == sys.argv[4] and i[9] == str(sys.argv[5]):
+            country.append(i)
+    return country
+def create_medals ():
+    bronze = 0
+    silver = 0
+    gold = 0
+    country = create_country()
+    for i in country:
+            if i[-1]=="Silver":
+                silver +=1
+            elif i[-1]=="Gold":
+                gold +=1
+            elif i[-1]=="Bronze":
+                bronze +=1
+    return bronze, silver, gold
